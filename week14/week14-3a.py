d6074df642c752afd55aa5bd2ba332faae6055eb
322025 <wangpinyaow@gmail.com>
2026-05-28 16:26:30 +0800

    week14

---
+++ week14/week14-3a.py
@@ -0,0 +1,9 @@
+# week14-3a.py 學習計畫 DP - 1D 第2題 Easy 題
+# LeetCode 746. Min Cost Climbing Stairs
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        @cache  # 函式呼叫函式, 把大問題, 拆成小問題
+        def helper(i):  # 現在踩在第 i 格, 之後要多少錢?
+            if i >= len(cost): return 0
+            return cost[i] + min( helper(i+1), helper(i+2) )
+        return min( helper(0), helper(1) )
